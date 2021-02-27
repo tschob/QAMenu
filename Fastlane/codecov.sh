ab5a95f46ed99456f730e5bb4c49b2a0e28917a2
Hans Seiffert <tschob@posteo.de>
2021-02-27 18:04:47 +0100

    Fix Slather

--- Fastlane/codecov.sh
+++ Fastlane/codecov.sh
@@ -5,17 +5,17 @@ set -e
 function run_for_framework() {
 	framework="$1"
 
-	echo "Exporting coverage and uploading it to Codecov for framework \"framework\""
+	echo "Exporting coverage and uploading it to Codecov for framework \"$framework\""
 
 	bundle exec slather coverage -x \
-		--source-directory "Sources/QAMenu" \
+		--source-directory "Sources/$framework" \
 		--output-directory ".build/Coverage/$framework" \
 		--scheme "AllUnitTests" \
 		--workspace "QAMenu.xcworkspace" \
 		--binary-basename "$framework" \
 		--ignore "Tests/*" \
-		--ignore "Sources/$framework/*" \
-		QAMenu.xcodeproj
+		--ignore "Sources/QAMenuCatalog/*" \
+		"QAMenu.xcodeproj"
 
 	bash <(curl -s https://codecov.io/bash) -f ".build/Coverage/$framework/cobertura.xml" -X coveragepy -X gcov -X xcode -F "$framework"
 }
@@ -23,6 +23,5 @@ function run_for_framework() {
 run_for_framework "QAMenu"
 run_for_framework "QAMenuUIKit"
 run_for_framework "QAMenuUtils"
-
 
 exit 0
